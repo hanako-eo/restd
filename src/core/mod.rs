ba526f75f1ea77586471fcc638332f4e20f4dfbb
hanako-eo <enuxura@gmail.com>
2024-02-11 01:01:16 +0100

    Start to implement ptr module and `NonNull<T>`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,6 @@
 pub mod option;
 pub mod result;
+pub mod ptr;
 
 #[macro_export]
 macro_rules! matches {
---
+++ src/core/ptr/non_null.rs
@@ -0,0 +1,202 @@
+use core::mem::MaybeUninit;
+
+use crate::core::option::Option;
+
+#[repr(transparent)]
+pub struct NonNull<T: ?Sized>(*mut T);
+
+impl<T: ?Sized> NonNull<T> {
+    pub const unsafe fn new_unchecked(ptr: *mut T) -> NonNull<T> {
+        NonNull(ptr)
+    }
+
+    pub fn new(ptr: *mut T) -> Option<NonNull<T>> {
+        if ptr.is_null() {
+            Option::None
+        } else {
+            Option::Some(unsafe { Self::new_unchecked(ptr) })
+        }
+    }
+
+    // TODO: MaybeUninit
+    pub unsafe fn as_uninit_ref<'a>(self) -> &'a MaybeUninit<T>
+    where
+        T: Sized
+    {
+        unsafe { &*(self.0 as *const MaybeUninit<T>) }
+    }
+    
+    pub unsafe fn as_uninit_mut<'a>(self) -> &'a mut MaybeUninit<T>
+    where
+        T: Sized
+    {
+        unsafe { &mut *(self.0 as *mut MaybeUninit<T>) }
+    }
+
+    pub fn addr(self) -> usize
+    where
+        T: Sized
+    {
+        self.0 as usize
+    }
+
+    pub const fn as_ptr(self) -> *mut T {
+        self.0
+    }
+
+    pub unsafe fn as_ref<'a>(self) -> &'a T {
+        unsafe { &*self.0 }
+    }
+
+    pub unsafe fn as_mut<'a>(self) -> &'a mut T {
+        unsafe { &mut *self.0 }
+    }
+
+    pub const unsafe fn cast<U>(self) -> NonNull<U> {
+        NonNull::new_unchecked(self.as_ptr() as *mut U)
+    }
+
+    pub const unsafe fn offset(self, count: isize) -> Self
+    where
+        T: Sized
+    {
+        NonNull::new_unchecked(self.0.offset(count))
+    }
+
+    // TODO: nightly
+    // pub unsafe fn byte_offset(self, count: isize) -> Self {
+    //     NonNull::new_unchecked(self.0.byte_offset(count))
+    // }
+
+    pub const unsafe fn add(self, count: usize) -> Self
+    where
+        T: Sized
+    {
+        NonNull::new_unchecked(self.0.add(count))
+    }
+
+    // TODO: nightly
+    // pub unsafe fn byte_add(self, count: usize) -> Self {
+    //     NonNull::new_unchecked(self.0.byte_add(count))
+    // }
+
+    pub const unsafe fn sub(self, count: usize) -> Self
+    where
+        T: Sized
+    {
+        NonNull::new_unchecked(self.0.sub(count))
+    }
+
+    // TODO: nightly
+    // pub unsafe fn byte_sub(self, count: usize) -> Self {
+    //     NonNull::new_unchecked(self.0.byte_sub(count))
+    // }
+    
+    pub const unsafe fn offset_from(self, origin: NonNull<T>) -> isize
+    where
+        T: Sized
+    {
+        self.0.offset_from(origin.as_ptr())
+    }
+    
+    // TODO: nightly
+    // pub unsafe fn sub_ptr(self, subtracted: NonNull<T>) -> usize
+    // where
+    //     T: Sized
+    // {
+    //     self.0.sub_ptr(subtracted.as_ptr())
+    // }
+
+    pub const unsafe fn read(self) -> T
+    where
+        T: Sized
+    {
+        self.0.read()
+    }
+
+    pub unsafe fn read_volatile(self) -> T
+    where
+        T: Sized
+    {
+        self.0.read_volatile()
+    }
+
+    pub const unsafe fn copy_to(self, dest: NonNull<T>, count: usize)
+    where
+        T: Sized
+    {
+        self.0.copy_to(dest.as_ptr(), count)
+    }
+    
+    pub unsafe fn copy_to_nonoverlapping(self, dest: NonNull<T>, count: usize)
+    where
+    T: Sized
+    {
+        self.0.copy_to_nonoverlapping(dest.as_ptr(), count)
+    }
+
+    pub const unsafe fn copy_from(self, dest: NonNull<T>, count: usize)
+    where
+        T: Sized
+    {
+        self.0.copy_from(dest.as_ptr(), count)
+    }
+
+    pub unsafe fn copy_from_nonoverlapping(self, dest: NonNull<T>, count: usize)
+    where
+        T: Sized
+    {
+        self.0.copy_from_nonoverlapping(dest.as_ptr(), count)
+    }
+
+    pub unsafe fn drop_in_place(self)
+    where
+        T: Sized
+    {
+        self.0.drop_in_place()
+    }
+
+    pub unsafe fn write(self, value: T)
+    where
+        T: Sized
+    {
+        self.0.write(value)
+    }
+
+    pub unsafe fn write_volatile(self, value: T)
+    where
+        T: Sized
+    {
+        self.0.write_volatile(value)
+    }
+    
+    pub unsafe fn write_unaligned(self, value: T)
+    where
+        T: Sized
+    {
+        self.0.write_unaligned(value)
+    }
+
+    pub unsafe fn replace(self, value: T) -> T
+    where
+        T: Sized
+    {
+        self.0.replace(value)
+    }
+
+    pub unsafe fn swap(self, with: NonNull<T>)
+    where
+        T: Sized
+    {
+        self.0.swap(with.as_ptr())
+    }
+}
+
+impl<T: ?Sized> Clone for NonNull<T> {
+    fn clone(&self) -> Self {
+        // SAFETY: if the clone is called, we can consider than the pointer is non-null
+        unsafe { NonNull::new_unchecked(self.0) }
+    }
+}
+
+impl<T: ?Sized> Copy for NonNull<T> { }
